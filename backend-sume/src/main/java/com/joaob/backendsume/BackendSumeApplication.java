49ac1b9d6a315172e1c17912f6546ed297149f48
JoaoLuizBorges <joaoluizosborges@gmail.com>
2024-03-07 20:09:15 -0300

    Adicionado o repositório do software

---
+++ backend-sume/src/main/java/com/joaob/backendsume/BackendSumeApplication.java
@@ -0,0 +1,13 @@
+package com.joaob.backendsume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSumeApplication.class, args);
+	}
+
+}
---
+++ backend-sume/src/test/java/com/joaob/backendsume/BackendSumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaob.backendsume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
